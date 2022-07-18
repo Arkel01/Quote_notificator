c071bc042cd21a791829aab4b3bfd3243a09eaa0
Arkel01 <sullivan.71@hotmail.fr>
2022-07-18 02:51:09 +0200

    Patch du batch compile_local

--- compile_local.bat
+++ compile_local.bat
@@ -2,5 +2,5 @@ set path_to_github_folder=PATH\TO\GITHUB\FOLDER\HERE
 
 IF EXIST Quote_notificator_local.js DEL /F Quote_notificator_local.js
 FINDSTR /R /I /V "^$// ==/UserScript==" userscript_header.js >> Quote_notificator_local.js
-echo // @require      file:///%path_to_github_folder%\functions\*.js >> Quote_notificator_local.js
+FOR %%f IN (functions\*.js) DO echo // @require      file:///%path_to_github_folder%\functions\%%~nxf >> Quote_notificator_local.js
 echo // ==/UserScript== >> Quote_notificator_local.js
