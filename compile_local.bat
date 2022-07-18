7e7bb0d428a74012616b126216fe2377e1bba336
Arkel01 <sullivan.71@hotmail.fr>
2022-07-18 11:56:38 +0200

    Mise à jour compile_local
    Le script local Quote_notificator_local a maintenant pour extension .user.js et peut être directement lancé dans le navigateur pour basculer sur la version locale du script

--- compile_local.bat
+++ compile_local.bat
@@ -1,4 +1,4 @@
-IF EXIST Quote_notificator_local.js DEL /F Quote_notificator_local.js
-FINDSTR /R /I /V "^$// ==/UserScript==" userscript_header.js >> Quote_notificator_local.js
-FOR %%f IN (functions\*.js) DO echo // @require      file:///%cd%\functions\%%~nxf >> Quote_notificator_local.js
-echo // ==/UserScript== >> Quote_notificator_local.js
+IF EXIST Quote_notificator_local.user.js DEL /F Quote_notificator_local.user.js
+FINDSTR /R /I /V "^$// ==/UserScript==" userscript_header.js >> Quote_notificator_local.user.js
+FOR %%f IN (functions\*.js) DO echo // @require      file:///%cd%\functions\%%~nxf >> Quote_notificator_local.user.js
+echo // ==/UserScript== >> Quote_notificator_local.user.js
